fb5b2221448e85e930fba570bc46b331b5e0b9f9
Nevroz Arslan <newrozarslan@gmail.com>
2023-01-27 22:52:42 +0300

    add board drawer

--- tests/run.scm
+++ tests/run.scm
@@ -1,6 +1,7 @@
 (import test
 	srfi-178
-	vector-lib ; vector library
+	(only vector-lib vector-for-each) ; vector library
+	
 	)
 
 (include-relative "../defboard.scm")
@@ -16,7 +17,23 @@
             (loop (cdr attr))))))
 
 (define (draw-vec board-vec)
-(vector-for-each (lambda (i x) (print i " " x)) board-vec
+  (vector-for-each (lambda (i x)
+		     (begin
+			   (case x
+			     ['blackPawn (display " ♟ ")]
+			     ['blackKnight (display " ♞ ")]
+			     ['blackBishop (display " ♝ ")]
+			     ['blackRook (display " ♜ ")]
+			     ['blackQueen (display " ♛ ")]
+			     ['blackKing (display " ♚ ")]
+			     [else (display " - ")]
+			     )
+			   (if
+			    (equal? (modulo (+ i 1) 8) 0)
+			    (newline)
+			    ))		     
+		     )
+		   board-vec
   ))
 
 (define (game-def)
@@ -24,9 +41,12 @@
 	 [board2 (make-bb)]
 	 [board3 (update-board board2 'a1 'blackRook)]
 	 [board4 (update-board board3 'h1 'blackPawn)]
-	 [board5 (update-board board4 'h8 'blackKnight)]
+	 [board5 (update-board board4 'e1 'blackKing)]
+	 [board6 (update-board board5 'h8 'blackKnight) ]
+	 [board7 (update-board board6 'e2 'blackQueen)]
+	 [board8 (update-board board7 'e4 'blackPawn)]
 	 )
-    (draw-vec (bbindex board5))))
+    (draw-vec (bbindex board8))))
 
 
 (game-def)
