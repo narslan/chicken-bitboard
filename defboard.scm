999fcbf89f366ee8b829ade7de23224bf260bbb0
Nevroz Arslan <newrozarslan@gmail.com>
2023-01-27 05:22:59 +0300

    add definiton for draw board

--- defboard.scm
+++ defboard.scm
@@ -28,9 +28,17 @@
     (bitvector-ior nbv bv)))
 
 
-(define lsquares  `(a1 a2 a3 a4 a5 a6 a7 a8 b1 b2 b3 b4 b5 b6 b7 b8 c1 c2 c3 c4 c5 c6 c7 c8 d1 d2 d3 d4 d5 d6 d7 d8 e1 e2 e3 e4 e5 e6 e7 e8 f1 f2 f3 f4 f5 f6 f7 f8 g1 g2 g3 g4 g5 g6 g7 g8 h1 h2 h3 h4 h5 h6 h7 h8))
+(define rsquares  `(a1 a2 a3 a4 a5 a6 a7 a8 b1 b2 b3 b4 b5 b6 b7 b8 c1 c2 c3 c4 c5 c6 c7 c8 d1 d2 d3 d4 d5 d6 d7 d8 e1 e2 e3 e4 e5 e6 e7 e8 f1 f2 f3 f4 f5 f6 f7 f8 g1 g2 g3 g4 g5 g6 g7 g8 h1 h2 h3 h4 h5 h6 h7 h8))
 
- ;; 'a1 -> 0 'h8-> 63
+(define llsquares `( a1 a2 b2 c2 d2 e2 f2 g2 h2
+  a3 b3 c3 d3 e3 f3 g3 h3
+  a4 b4 c4 d4 e4 f4 g4 h4
+  a5 b5 c5 d5 e5 f5 g5 h5
+  a6 b6 c6 d6 e6 f6 g6 h6
+  a7 b7 c7 d7 e7 f7 g7 h7
+  a8 b8 c8 d8 e8 f8 g8 h8))
+(define lsquares (reverse llsquares))
+;; 'a1 -> 0 'h8-> 63
 (: square->index (symbol --> fixnum))
 (define (square->index square)
   (list-index (lambda (x) (equal? square x )) lsquares))
@@ -48,6 +56,19 @@
   (whiteKing (make-bitvector 64 0) )
   (whiteAll (make-bitvector 64 0) ))
 
+(define (bbindex board)
+  (let loop ((attr (bb->alist t))
+	     
+	     )
+    (cond ((null? attr) 'done)
+          (else
+            (display (caar attr))
+	    (display ": ")
+            (display (bitvector->string (cdar attr)))
+	    (newline)
+            (loop (cdr attr) ))))
+  
+  )
 
 ;; functionally update 
 (define (updateBlackAll board square)
--- tests/run.scm
+++ tests/run.scm
@@ -18,8 +18,8 @@
   (let loop ((attr (bb->alist t)))
     (cond ((null? attr) 'done)
           (else
-            (display (caar attr)
-		     )(display ": ")
+            (display (caar attr))
+	    (display ": ")
             (display (bitvector->string (cdar attr)))(newline)
             (loop (cdr attr))))))
 
@@ -28,9 +28,9 @@
 (define (game-def)
   (let* (
 	 [board2 (make-bb)]
-	 [board3 (update-board board2 'h8 'blackRook)]
-	 [board4 (update-board board3 'h7 'blackPawn)]
-	 [board5 (update-board board4 'h6 'blackRook)])
+	 [board3 (update-board board2 'a1 'blackRook)]
+	 [board4 (update-board board3 'a2 'blackPawn)]
+	 [board5 (update-board board4 'h8 'blackRook)])
     (pp-tree board5)))
 
 (game-def)
